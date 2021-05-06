7d6a467394349661fd96332188d0b96fd6e5c59e
SkqLiao <skqliao@gmail.com>
2021-05-06 23:37:21 +0800

    update

--- Two Pointers Method/Step 3/i.cpp
+++ Two Pointers Method/Step 3/i.cpp
@@ -1,28 +1,64 @@
 /*
- * @date:2021-04-28 14:34:28
+ * @date:2021-04-28 16:14:21
  * @source:https://codeforces.com/edu/course/2/lesson/9/3/practice/contest/307094/problem/I
 */
 #include <bits/stdc++.h>
 
 using namespace std;
 
+const int MAXM = 1005;
+
 int n, m;
-vector<int> v, F;
+vector<int> v;
+
+struct Stack{
+    vector<int> v;
+    vector<bitset<MAXM>> val;
+    void init(int x) {
+        bitset<MAXM> b = 0;
+        b[x] = 1;
+        val.push_back(b);
+    }
+    void push(int x, int t) {
+        if (t) val.push_back(val.back() | val.back() << x);
+        else val.push_back(val.back() | val.back() >> x);
+        v.push_back(x);
+    }
+    int pop() {
+        int x = v.back();
+        val.pop_back();
+        v.pop_back();
+        return x;
+    }
+    bitset<MAXM> top() {
+        return val.back();
+    }
+    bool empty() {
+        return v.empty();
+    }
+} s1, s2;
+
+void remove() {
+    if (s1.empty()) {
+        while (!s2.empty()) s1.push(s2.pop(), 1);
+    }
+    s1.pop();
+}
 
 int main() {
     scanf("%d%d", &n, &m);
     v.resize(n);
-    F.resize(m);
     for (auto &x : v) scanf("%d", &x);
-    fill_n(F.begin(), m, -1e9);
-    int ans = n + 1;
-    for (int i = 0; i < n; ++i) {
-        if (v[i] == m) ans = 1;
-        ans = min(ans, i - F[m - v[i]] + 1);
-        for (int j = m - 1; j > v[i]; --j) {
-            F[j] = max(F[j], F[j - v[i]]);
+    int l = 0, ans = n + 1;
+    s1.init(0);
+    s2.init(m);
+    for (int r = 0; r < n; ++r) {
+        s2.push(v[r], 0);
+        while ((s1.top() & s2.top()).any()) {
+            remove();
+            ans = min(ans, r - l + 1);
+            l++;
         }
-        F[v[i]] = i;
     }
     if (ans > n) ans = -1;
     printf("%d\n", ans);
