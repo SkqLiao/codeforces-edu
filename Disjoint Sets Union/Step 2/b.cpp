2ffdbc19844b6e54043ddf497a0fa1ed7535be32
SkqLiao <skqliao@gmail.com>
2021-05-07 00:28:46 +0800

    update 2-b

--- Disjoint Sets Union/Step 2/b.cpp
+++ Disjoint Sets Union/Step 2/b.cpp
@@ -8,17 +8,38 @@ using namespace std;
 
 const int MAXN = 300000 + 5;
 
-int A[MAXN << 2];
+int n;
+int A[MAXN], R[MAXN], Fa[MAXN];
+
+int findFa(int x) {
+    return Fa[x] == x ? x : Fa[x] = findFa(Fa[x]);
+}
+
+int getL(int x) {
+    return x == 1 ? n : x - 1;
+}
+
+int getR(int x) {
+    return x == n ? 1 : x + 1;
+}
 
 int main() {
     scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+        R[i] = Fa[i] = i;
+    }
     for (int i = 1, x; i <= n; ++i) {
         scanf("%d", &x);
-        if (!A[x] && !A[x + n]) {
-            printf("%d\n", x);
-            A[x] = 1;
-            if (A[x - 1]) merge(x, x - 1);
-            if (A[x + 1]) merge(x, x + 1);
+        x = R[findFa(x)];
+        printf("%d ", x);
+        A[x] = 1;
+        if (A[getR(x)]) {
+            Fa[x] = findFa(getR(x));
+        } else {
+            R[x] = getR(x);
+        }
+        if (A[getL(x)]) {
+            Fa[findFa(getL(x))] = Fa[x];
         }
     }
     return 0;
